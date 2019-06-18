5d820d994ee6fd94c48b8073a09d19d8fe341971
akulbasov <akulbasov@gmail.com>
2019-06-18 16:33:33 +0300

    -- part 20 gridu

--- dags/jobs_dag.py
+++ dags/jobs_dag.py
@@ -1,78 +1,89 @@
 from airflow import DAG
 from datetime import datetime
+
+from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
-import datetime
+from datetime import datetime, timedelta
+from airflow.operators.python_operator import PythonOperator, BranchPythonOperator
+from airflow.utils.trigger_rule import TriggerRule
 
-from airflow.operators.python_operator import PythonOperator
-
+list_of_table_names = ["table_1", "table_2", "table_3", "table_4"]
 
 def get_current_date():
-    return datetime.datetime.now()
+    return datetime.now()
 
-
-config_for_all_dag = {
-    'dag_id_1': {
-        'start_date': get_current_date(),
-        'table_name': "table_name_1"
-    },
-    'dag_id_2': {
-        'start_date': get_current_date(),
-        'table_name': "table_name_2"
-    },
-    'dag_id_3': {
-        'start_date': get_current_date(),
-        'table_name': "table_name_3"
-    }
-}
 
 # [START howto_operator_python]
-def print_context(ds, **kwargs):
+def print_process_start(ds, **kwargs):
     """Print the Airflow context and ds variable from the context."""
-    print(kwargs)
-    print(ds)
     return 'start processing tables in database: {mock_database}'
 
 
-config_dag_1 = config_for_all_dag["dag_id_1"]
-config_dag_2 = config_for_all_dag["dag_id_3"]
-config_dag_3 = config_for_all_dag["dag_id_3"]
+def create_table():
+    return 'Created table in db'
 
 
 default_args = {
     "owner": "akulbasov"
 }
-dag_1 = DAG(
-    start_date=config_dag_1["start_date"],
-    dag_id="dag_id_1",
-    default_args=default_args
+
+dag = DAG(
+    start_date=get_current_date(),
+    dag_id="processing_with_database",
+    default_args=default_args,
+    schedule_interval=timedelta(microseconds=1)
 )
 
-task_1 = PythonOperator(
-    op_kwargs="dag_id_1",
-    task_id='print_in_the_log',
+start_task = PythonOperator(
+    task_id='print_process_start',
     provide_context=True,
-    python_callable=print_context,
-    dag=dag_1
+    python_callable=print_process_start,
+    dag=dag
 )
 
-# dag_2 = DAG(
-#     start_date=config_dag_2["start_date"],
-#     dag_id="dag_id_2",
-# )
 
-task_2 = DummyOperator(task_id='insert_new_row', dag=dag_1)
-
-
-# dag_3 = DAG(
-#     start_date=config_dag_3["start_date"],
-#     dag_id="dag_id_3",
-# )
-
-task_3 = DummyOperator(task_id='query_the_table', dag=dag_1)
+create_table = PythonOperator(
+    task_id='create_table',
+    provide_context=True,
+    python_callable=create_table,
+    dag=dag
+)
 
 
-task_1 >> task_2 >> task_3
+bash_command = """
+echo "$USER"
+"""
 
+echo_user = BashOperator(
+    task_id='execute_bash_command',
+    bash_command='echo $USER',
+    dag=dag
+)
+
+
+
+dummy = DummyOperator(task_id='dummy_task', dag=dag, depends_on_past=False)
+
+
+def check_table_exist(**kwargs):
+    import random
+    if random.sample(range(1, 3), 1) == 1:
+        return 'dummy_task'
+    else:
+        return 'create_table'
+
+check_table_exist = BranchPythonOperator(
+    task_id='check_table_exist',
+    python_callable=check_table_exist,
+    provide_context=True,
+    dag=dag
+)
+
+insert_new_row = DummyOperator(task_id='insert_new_row', dag=dag, trigger_rule=TriggerRule.ALL_DONE)
+query_the_table = DummyOperator(task_id='query_the_table', dag=dag)
+
+
+start_task >> echo_user >> check_table_exist >> [dummy, create_table] >> insert_new_row >> query_the_table
 
 
 
